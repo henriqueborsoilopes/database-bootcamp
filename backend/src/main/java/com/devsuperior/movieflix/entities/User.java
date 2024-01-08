877f1d79821b557fd0dc952a086664f988ee5ed6
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-08 15:42:16 -0300

    feat(User): implements UserDetails interface

--- backend/src/main/java/com/devsuperior/movieflix/entities/User.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -2,12 +2,15 @@ package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,9 +20,13 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-@Table(name = "tb_review")
-public class User implements Serializable {
+@Table(name = "tb_user")
+public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -29,7 +36,7 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 		joinColumns = @JoinColumn(name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -94,6 +101,36 @@ public class User implements Serializable {
 
 	public void addReview(Review review) {
 		this.reviews.add(review);
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map((role) -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 	@Override
