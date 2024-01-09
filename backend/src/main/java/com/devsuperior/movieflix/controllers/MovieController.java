d2d41c129a82b28b62dcfb947e020bfaaacfb0cc
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-09 15:26:33 -0300

    feat(MovieController): findByGenre method

--- backend/src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,10 +1,13 @@
 package com.devsuperior.movieflix.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dtos.MovieDTO;
@@ -25,5 +28,14 @@ public class MovieController {
 	public ResponseEntity<MovieDTO> findById(@PathVariable("id") Long id) {
 		MovieDTO dto = movieService.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PreAuthorize("hasAnyRole('MEMBER', 'VISITOR')")
+	@GetMapping
+	public ResponseEntity<Page<MovieDTO>> findByGenre(
+			@RequestParam(value = "genreId", defaultValue = "0") Long genreId, 
+			Pageable pageable) {
+		Page<MovieDTO> page = movieService.findByGenre(genreId, pageable);
+		return ResponseEntity.ok().body(page);
 	}
 }
--- backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -2,11 +2,16 @@ package com.devsuperior.movieflix.services;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dtos.MovieDTO;
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.mapper.MovieMapper;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.services.exceptions.ObjectNotFoundException;
 
@@ -14,16 +19,28 @@ import com.devsuperior.movieflix.services.exceptions.ObjectNotFoundException;
 public class MovieService {
 
 	private final MovieRepository movieRepository;
+	private final GenreRepository genreRepository;
 	private final MovieMapper mapper;
 	
-	public MovieService(MovieRepository movieRepository, MovieMapper mapper) {
+	public MovieService(MovieRepository movieRepository, MovieMapper mapper, GenreRepository genreRepository) {
 		this.movieRepository = movieRepository;
+		this.genreRepository = genreRepository;
 		this.mapper = mapper;
 	}
 
+
+	@Transactional(readOnly = true)
 	public MovieDTO findById(Long id) {
 		Optional<Movie> optional = movieRepository.findById(id);
 		Movie entity = optional.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 		return mapper.toDTO(entity);
+	}
+
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findByGenre(Long genreId, Pageable pageable) {
+		Genre genre = genreId == 0 ? null : genreRepository.getReferenceById(genreId);
+		Page<Movie> page = movieRepository.findByGenre(genre, pageable);
+		return page.map((entity) -> mapper.toDTO(entity));
 	}
 }
