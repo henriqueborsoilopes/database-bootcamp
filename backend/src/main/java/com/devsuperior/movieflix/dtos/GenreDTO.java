6491462027b36cda263cac9de1b4e8198ef1a5ac
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-08 21:06:11 -0300

    feat(GenreDTO): data transfer object

---
+++ backend/src/main/java/com/devsuperior/movieflix/dtos/GenreDTO.java
@@ -0,0 +1,33 @@
+package com.devsuperior.movieflix.dtos;
+
+import java.io.Serializable;
+
+public class GenreDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	public GenreDTO() { }
+
+	public GenreDTO(Long id, String name) {
+		super();
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
